6815c40ab8c8809b6ce310db69231d1944e98093
Atharva69669 <116868631+Atharva69669@users.noreply.github.com>
2023-07-20 15:13:23 +0530

    Update Bill.jsx

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -2,37 +2,40 @@ import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import { collection, query, where, getDocs } from "firebase/firestore";
 
-const Bill = ({ props }) => {
-    const { pro_id, onDeleteProduct, onUpdateSubtotal } = props;
-
+const Bill = (props) => {
     const [productData, setProductData] = useState(null);
     const [Subtotal, setSubtotal] = useState(1);
     const [state, setState] = useState(true);
 
+    async function getData() {
+        const collectionRef = collection(db, "Database");
+        const id = parseInt(props.props.pro_id, 10);
+        const q = query(collectionRef, where("productId", "==", id));
+        const docSnap = await getDocs(q);
+        docSnap.forEach((product) => {
+            setProductData(product.data());
+        });
+    }
+
+    const handleDelete = () => {
+        props.onDeleteProduct(productData.productId);
+        setState(!state);
+    };
+
     useEffect(() => {
-        async function getData() {
-            const collectionRef = collection(db, "Database");
-            const id = parseInt(pro_id, 10);
-            const q = query(collectionRef, where("productId", "==", id));
-            const docSnap = await getDocs(q);
-            docSnap.forEach((product) => {
-                setProductData(product.data());
-            });
-        }
         getData();
-    }, [pro_id]);
+    }, []);
 
     useEffect(() => {
         // Make sure productData is available before calculating subtotal
         if (productData) {
-            onUpdateSubtotal(Subtotal * productData.productPrice, productData.productId);
+            props.onUpdateSubtotal(Subtotal * productData.productPrice, productData.productId);
         }
-    }, [Subtotal, productData, onUpdateSubtotal]);
+    }, [Subtotal, productData]);
 
     if (!productData || !state) {
         return null;
     }
-
 
     return (
         state && (
