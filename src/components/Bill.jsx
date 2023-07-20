bbd138f78f8f45b1ce1442a044fd0b3efd94f3ec
Atharva69669 <116868631+Atharva69669@users.noreply.github.com>
2023-07-20 16:07:40 +0530

    Update Bill.jsx

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,72 +1,72 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 const Bill = (props) => {
-    const { pro_id, onDeleteProduct, onUpdateSubtotal } = props;
 
     const [productData, setProductData] = useState(null);
     const [Subtotal, setSubtotal] = useState(1);
     const [state, setState] = useState(true);
 
-    const getData = useCallback(async () => {
+    async function getData() {
         const collectionRef = collection(db, "Database");
-        const id = parseInt(pro_id, 10);
+        const id = parseInt(props.props.pro_id, 10);
         const q = query(collectionRef, where("productId", "==", id));
         const docSnap = await getDocs(q);
         docSnap.forEach((product) => {
             setProductData(product.data());
-            console.log(product.data());
         });
-    }, [pro_id]);
-
+    }
     const handleDelete = () => {
-        onDeleteProduct(productData.productId);
+        props.onDeleteProduct(productData.productId);
+      
         setState(!state);
-    };
-
+      };
     useEffect(() => {
         getData();
-    }, [getData]);
+    }, []);
 
     useEffect(() => {
         // Make sure productData is available before calculating subtotal
         if (productData) {
-            onUpdateSubtotal(Subtotal * productData.productPrice, productData.productId);
+            props.onUpdateSubtotal(Subtotal * productData.productPrice,productData.productId);
         }
-    }, [Subtotal, productData, onUpdateSubtotal]);
+    }, [Subtotal, productData,]);
 
-    if (!productData || !state) {
-        return null;
-    }
+   
+  if (!productData || !state) {
+    return null;
+  }
 
-   return( 
-       state && 
-    <tbody>
-        <tr>
-            <td data-th="Product">
-                <div className="row">
-                    <div className="col-sm-2 hidden-xs">
-                    </div>
-                    <div className="col-sm-10">
-                        <h4 className="nomargin">{productData.productName}</h4>
-                        <p>{productData.ProductDesc}</p>
-                    </div>
-                </div>
-            </td>
-            <td data-th="Price">${productData.productPrice}</td>
-            <td data-th="Quantity">
-                <input type="number" value={Subtotal} className="form-control text-center" onChange={(e) => { setSubtotal(e.target.value) }} />
-            </td>
-            <td data-th="Subtotal" className="text-center">${Subtotal * productData.productPrice}</td>
-            <td className="actions" data-th="">
-                <button className="btn btn-danger btn-sm" onClick={handleDelete}>
-                    <i className="fa fa-trash-o"></i>
-                </button>
-            </td>
-        </tr>
-    </tbody>
-);
+
+    return(
+        state && 
+            <tbody>
+                <tr>
+                    <td data-th="Product">
+                        <div className="row">
+                            <div className="col-sm-2 hidden-xs">
+                            </div>
+                            <div className="col-sm-10">
+                                <h4 className="nomargin">{productData.productName}</h4>
+                                <p>{productData.ProductDesc}</p>
+                            </div>
+                        </div>
+                    </td>
+                    <td data-th="Price">${productData.productPrice}</td>
+                    <td data-th="Quantity">
+                        <input type="number" value={Subtotal} className="form-control text-center" onChange={(e) => { setSubtotal(e.target.value) }} />
+                    </td>
+                    <td data-th="Subtotal" className="text-center">${Subtotal * productData.productPrice}</td>
+                    <td className="actions" data-th="">
+                        <button className="btn btn-danger btn-sm" onClick={handleDelete}>
+                            <i className="fa fa-trash-o"></i>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        
+    )
 
 };
 
