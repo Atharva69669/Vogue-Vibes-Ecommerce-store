e511def316eb219cd342c52e0f187347394ccb9f
Atharva69669 <116868631+Atharva69669@users.noreply.github.com>
2023-07-20 14:34:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -16,4 +15,3 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db=getFirestore(app);
-const analytics = getAnalytics(app);
